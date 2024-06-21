a7047014d4f8c0bd386d9a1aaad8c19bd17de24e
zhouchaoquan <77728751+zhouchaoquan@users.noreply.github.com>
2024-06-22 00:37:11 +0800

    Update MenuList.js

--- themes/example/components/MenuList.js
+++ themes/example/components/MenuList.js
@@ -1,57 +1,45 @@
-import Collapse from '@/components/Collapse'
 import { siteConfig } from '@/lib/config'
 import { useGlobal } from '@/lib/global'
-import { useRouter } from 'next/router'
-import { useEffect, useRef, useState } from 'react'
 import CONFIG from '../config'
-import { MenuItemCollapse } from './MenuItemCollapse'
 import { MenuItemDrop } from './MenuItemDrop'
 
 /**
- * 菜单导航
+ * 导航菜单列表
  * @param {*} props
  * @returns
  */
-export const MenuList = ({ customNav, customMenu }) => {
+export const MenuList = props => {
+  const { customNav, customMenu } = props
   const { locale } = useGlobal()
-  const [isOpen, changeIsOpen] = useState(false)
-  const toggleIsOpen = () => {
-    changeIsOpen(!isOpen)
-  }
-  const closeMenu = e => {
-    changeIsOpen(false)
-  }
-  const router = useRouter()
-  const collapseRef = useRef(null)
-
-  useEffect(() => {
-    router.events.on('routeChangeStart', closeMenu)
-  })
 
   let links = [
     {
+      id: 1,
       icon: 'fas fa-search',
       name: locale.NAV.SEARCH,
       href: '/search',
-      show: siteConfig('SIMPLE_MENU_SEARCH', null, CONFIG)
+      show: siteConfig('EXAMPLE_MENU_SEARCH', null, CONFIG)
     },
     {
+      id: 2,
       icon: 'fas fa-archive',
       name: locale.NAV.ARCHIVE,
       href: '/archive',
-      show: siteConfig('SIMPLE_MENU_ARCHIVE', null, CONFIG)
+      show: siteConfig('EXAMPLE_MENU_ARCHIVE', null, CONFIG)
     },
     {
+      id: 3,
       icon: 'fas fa-folder',
       name: locale.COMMON.CATEGORY,
       href: '/category',
-      show: siteConfig('SIMPLE_MENU_CATEGORY', null, CONFIG)
+      show: siteConfig('EXAMPLE_MENU_CATEGORY', null, CONFIG)
     },
     {
+      id: 4,
       icon: 'fas fa-tag',
       name: locale.COMMON.TAGS,
       href: '/tag',
-      show: siteConfig('SIMPLE_MENU_TAG', null, CONFIG)
+      show: siteConfig('EXAMPLE_MENU_TAG', null, CONFIG)
     }
   ]
 
@@ -59,7 +47,7 @@ export const MenuList = ({ customNav, customMenu }) => {
     links = links.concat(customNav)
   }
 
-  // 如果 开启自定义菜单，则覆盖Page生成的菜单
+  // 如果 开启自定义菜单，则不再使用 Page生成菜单。
   if (siteConfig('CUSTOM_MENU')) {
     links = customMenu
   }
@@ -69,45 +57,17 @@ export const MenuList = ({ customNav, customMenu }) => {
   }
 
   return (
-    <>
-      {/* 大屏模式菜单 */}
-      <div id='nav-menu-pc' className='hidden md:flex my-auto'>
-        {links?.map((link, index) => (
-          <MenuItemDrop key={index} link={link} />
-        ))}
+    <nav className='w-full bg-white md:pt-0 px-6 relative z-20 border-t border-b border-gray-light dark:border-hexo-black-gray dark:bg-black'>
+      <div className='mx-auto max-w-4xl md:flex justify-between items-center text-sm md:text-md md:justify-start'>
+        <ul className='w-full text-center md:text-left flex flex-wrap justify-center items-stretch md:justify-start md:items-start'>
+          {links.map((link, index) => (
+            <MenuItemDrop key={index} link={link} />
+          ))}
+        </ul>
+        {/* <div className="w-full md:w-1/3 text-center md:text-right"> */}
+        {/* <!-- extra links --> */}
+        {/* </div> */}
       </div>
-      {/* 移动端小屏菜单 */}
-      <div
-        id='nav-menu-mobile'
-        className='flex md:hidden my-auto justify-start'>
-        <div
-          onClick={toggleIsOpen}
-          className='cursor-pointer hover:text-red-400 transition-all duration-200'>
-          <i
-            className={`${isOpen && 'rotate-90'} transition-all duration-200 fa fa-bars mr-3`}
-          />
-          <span>{!isOpen ? 'MENU' : 'CLOSE'}</span>
-        </div>
-
-        <Collapse
-          collapseRef={collapseRef}
-          className='absolute w-full top-12 left-0'
-          isOpen={isOpen}>
-          <div
-            id='menu-wrap'
-            className='bg-white dark:border-hexo-black-gray border'>
-            {links?.map((link, index) => (
-              <MenuItemCollapse
-                key={index}
-                link={link}
-                onHeightChange={param =>
-                  collapseRef.current?.updateCollapseHeight(param)
-                }
-              />
-            ))}
-          </div>
-        </Collapse>
-      </div>
-    </>
+    </nav>
   )
 }
