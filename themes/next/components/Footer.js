917b56d14029c3da52d6a932884b32be30ebb770
zhouchaoquan <77728751+zhouchaoquan@users.noreply.github.com>
2024-07-09 18:41:38 +0800

    Update Footer.js
    chrtgpt修改后

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -8,28 +8,63 @@ const Footer = ({ title }) => {
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-        <footer
-            className='relative z-10 dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm p-6 bg-white dark:text-gray-400'
-        >
+    <footer className='relative z-10 dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm p-6 bg-white dark:text-gray-400'>
+      <span>
+        <DarkModeButton />
+        <i className='fas fa-copyright' /> {`${copyrightDate}`}{' '}
+        <span className='mx-1 animate-pulse'>
+          <i className='fas fa-heart' />
+        </span>{' '}
+        <a href={siteConfig('LINK')} className='underline font-bold'>
+          {siteConfig('AUTHOR')}
+        </a>
+        .<br />
 
-            <span>
-                <DarkModeButton />
+        {/* 图片和备案信息在同一行 */}
+        <div className="flex items-center">
+          {/* 图片部分 */}
+          <img src='https://beian.mps.gov.cn/img/logo01.dd7ff50e.png' alt='备案图片' className='my-custom-class mr-2' />
+          {/* WANG_AN 备案信息 */}
+          {siteConfig('WANG_AN') && (
+            <>
+              <i className='' />{' '}
+              <a href='https://beian.mps.gov.cn/#/query/webSearch?code=33030402001375' className='mr-2'>
+                {siteConfig('WANG_AN')}
+              </a>
+            </>
+          )}
+        </div>
+        <br />
 
-                <i className='fas fa-copyright' /> {`${copyrightDate}`} <span className='mx-1 animate-pulse'><i className='fas fa-heart' /></span> <a href={siteConfig('LINK')} className='underline font-bold '>{siteConfig('AUTHOR')}</a>.<br />
+        {/* 其他备案信息 */}
+        {siteConfig('BEI_AN') && (
+          <>
+            <i className='' />{' '}
+            <a href='https://beian.miit.gov.cn/' className='mr-2'>
+              {siteConfig('BEI_AN')}
+            </a>
+            <br />
+          </>
+        )}
 
-                {siteConfig('WANG_AN') && <><i className='' /> <img src='https://beian.mps.gov.cn/img/logo01.dd7ff50e.png' alt='备案图片' className='my-custom-class' /><a href='https://beian.mps.gov.cn/#/query/webSearch?code=33030402001375' className='mr-2'>{siteConfig('WANG_AN')}</a><br /></>}
-              
-                {siteConfig('BEI_AN') && <><i className='' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br /></>}
-              
-                <span className='hidden busuanzi_container_site_pv'>
-                    <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span> </span>
-                <span className='pl-2 hidden busuanzi_container_site_uv'>
-                    <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-                <br />
-                <h1>{title}</h1>
-                <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {siteConfig('VERSION')}</a>.</span>
-            </span>
-        </footer>
+        <span className='hidden busuanzi_container_site_pv'>
+          <i className='fas fa-eye' />
+          <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
+        </span>
+        <span className='pl-2 hidden busuanzi_container_site_uv'>
+          <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
+        </span>
+        <br />
+        <h1>{title}</h1>
+        <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>
+          Powered by{' '}
+          <a href='https://github.com/tangly1024/NotionNext' className='underline '>
+            NotionNext {siteConfig('VERSION')}
+          </a>
+          .
+        </span>
+      </span>
+    </footer>
   )
 }
 
